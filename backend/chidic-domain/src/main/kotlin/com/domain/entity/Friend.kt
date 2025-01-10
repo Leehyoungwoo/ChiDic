411c99eeb32a4f69ba76339645c02e5679dbcfa4
dlguddn3024 <dlguddn3024@gmail.com>
2025-01-10 22:53:14 +0900

    fix: id filed GenerationType to Identity

--- backend/chidic-domain/src/main/kotlin/com/domain/entity/Friend.kt
+++ backend/chidic-domain/src/main/kotlin/com/domain/entity/Friend.kt
@@ -6,7 +6,7 @@ import jakarta.persistence.*
 @Entity
 class Friend (
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
 
     @ManyToOne(fetch = FetchType.LAZY)
