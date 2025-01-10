0cac3f2975227c0d3823ba4651f2ff4edb55e1cc
dlguddn3024 <dlguddn3024@gmail.com>
2025-01-10 22:51:56 +0900

    feat: Add Friend Entity and FriendStatus Enum

---
+++ backend/chidic-domain/src/main/kotlin/com/domain/entity/Friend.kt
@@ -0,0 +1,23 @@
+package com.domain.entity
+
+import com.domain.entity.enum.FriendStatus
+import jakarta.persistence.*
+
+@Entity
+class Friend (
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    var user: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "friend_id", nullable = false)
+    var friend: Friend,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    var friendStatus: FriendStatus
+)
