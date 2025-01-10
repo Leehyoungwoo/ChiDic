361c580f8c8aa8eab454e7aa0111609cec359e54
dlguddn3024 <dlguddn3024@gmail.com>
2025-01-10 23:24:46 +0900

    feat: Add Comment Entity

---
+++ backend/chidic-domain/src/main/kotlin/com/domain/entity/Comment.kt
@@ -0,0 +1,21 @@
+package com.domain.entity
+
+import jakarta.persistence.*
+
+@Entity
+class Comment (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feedpost_id", nullable = false)
+    var feedpost: FeedPost,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    var user: User,
+
+    @Column(name = "content", nullable = false, length = 255)
+    var content: String
+)
