f80ba4e85a2da7cf48961b37d4b294b078baa9d7
dlguddn3024 <dlguddn3024@gmail.com>
2025-01-10 22:21:19 +0900

    feat: Add BaseEntity with common fields

---
+++ backend/chidic-domain/src/main/kotlin/com/domain/entity/common/BaseEntity.kt
@@ -0,0 +1,31 @@
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@MappedSuperclass
+abstract class BaseEntity {
+    @Column(nullable = false)
+    var isDeleted: Boolean = false
+        protected set
+
+    @Column(updatable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now()
+        protected set
+
+    var updatedAt: LocalDateTime? = null
+        protected set
+
+    @PrePersist
+    fun prePersist() {
+        this.isDeleted = false
+        this.createdAt = LocalDateTime.now()
+    }
+
+    @PreUpdate
+    fun preUpdate() {
+        this.updatedAt = LocalDateTime.now()
+    }
+
+    fun deleteData() {
+        this.isDeleted = true
+    }
+}
