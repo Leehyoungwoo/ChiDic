433ca5cc1120b9c9bb6b883dc6a758dbf6e5e9d2
dlguddn3024 <dlguddn3024@gmail.com>
2025-01-10 21:23:30 +0900

    feat: first init

---
+++ backend/src/main/kotlin/com/chidic/ChidicApplication.kt
@@ -0,0 +1,11 @@
+package com.chidic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChidicApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChidicApplication>(*args)
+}
