79272ff9f940eb4ea7d097e7513c61b8a182ee9f
dlguddn3024 <dlguddn3024@gmail.com>
2025-01-10 22:55:31 +0900

    feat: Add Block Entity

---
+++ backend/chidic-domain/src/main/kotlin/com/domain/entity/Block.kt
@@ -0,0 +1,18 @@
+package com.domain.entity
+
+import jakarta.persistence.*
+
+@Entity
+class Block(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    var user: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "blocked_user_id", nullable = false)
+    var blockedUser: User
+)
