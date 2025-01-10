ea96b68d5f25ce8632e53a8a009382720643d009
dlguddn3024 <dlguddn3024@gmail.com>
2025-01-11 01:08:56 +0900

    feat: add GroupChatReadStatus Entity

---
+++ backend/chidic-domain/src/main/kotlin/com/domain/entity/GroupChatReadStatus.kt
@@ -0,0 +1,18 @@
+package com.domain.entity
+
+import jakarta.persistence.*
+
+@Entity
+class GroupChatReadStatus (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "groupchat_id", nullable = false)
+    var groupChat: GroupChat,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    var user: User,
+)
