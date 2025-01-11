4f47a80a5bf786326893c7158c3a181afd3a0568
dlguddn3024 <dlguddn3024@gmail.com>
2025-01-11 19:10:37 +0900

    feat: add common module

---
+++ backend/chidic-common/src/test/kotlin/com/chidiccommon/ChidicCommonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chidiccommon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChidicCommonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
