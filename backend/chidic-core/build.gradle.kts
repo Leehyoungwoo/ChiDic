855a5c1970879cdf1f28b1c16e35898d10cc07c9
dlguddn3024 <dlguddn3024@gmail.com>
2025-01-15 21:49:11 +0900

    feat: Implement OAuth2UserDetails class for OAuth2 user authentication

--- backend/chidic-core/build.gradle.kts
+++ backend/chidic-core/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 }
 
 dependencies {
+	implementation(project(":chidic-domain"))
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	testImplementation("org.springframework.security:spring-security-test")
