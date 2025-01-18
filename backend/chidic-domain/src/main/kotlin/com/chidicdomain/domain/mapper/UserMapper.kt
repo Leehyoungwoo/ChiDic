e7dd548c7a124a0bc2af049f44269f145ef7f182
dlguddn3024 <dlguddn3024@gmail.com>
2025-01-18 21:21:28 +0900

    feat: Implement UserMapper to mapping provider when making User

--- backend/chidic-domain/src/main/kotlin/com/chidicdomain/domain/mapper/UserMapper.kt
+++ backend/chidic-domain/src/main/kotlin/com/chidicdomain/domain/mapper/UserMapper.kt
@@ -1,13 +1,9 @@
 package com.chidicdomain.domain.mapper
 
 import com.chidiccommon.dto.OAuth2UserInfo
+import com.chidiccommon.enum.Provider
 import com.chidicdomain.domain.entity.User
-import org.mapstruct.Mapper
-import org.mapstruct.Mapping
 
-@Mapper(componentModel = "spring")
 interface UserMapper {
-    @Mapping(source = "email", target = "email")
-    @Mapping(source = "username", target = "username")
-    fun toEntity(oAuth2UserInfo: OAuth2UserInfo): User
+    fun toEntity(oAuth2UserInfo: OAuth2UserInfo, provider: Provider): User
 }
