7e2afdb5d55d62f5b5f1154f94e435cb7a7fa1c6
dlguddn3024 <dlguddn3024@gmail.com>
2025-01-19 19:48:25 +0900

    feat: Add Method to mapping dto from user

--- backend/chidic-domain/src/main/kotlin/com/chidicdomain/domain/mapper/UserMapper.kt
+++ backend/chidic-domain/src/main/kotlin/com/chidicdomain/domain/mapper/UserMapper.kt
@@ -1,9 +1,11 @@
 package com.chidicdomain.domain.mapper
 
 import com.chidiccommon.dto.OAuth2UserInfo
+import com.chidiccommon.dto.UserInfoResponse
 import com.chidiccommon.enum.Provider
 import com.chidicdomain.domain.entity.User
 
 interface UserMapper {
     fun toEntity(oAuth2UserInfo: OAuth2UserInfo, provider: Provider): User
+    fun toInfoDto(user: User): UserInfoResponse
 }
