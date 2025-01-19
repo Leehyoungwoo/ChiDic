e84bb1bcccf92ab94bd05d9410a0e4a6c546dffc
dlguddn3024 <dlguddn3024@gmail.com>
2025-01-19 21:56:26 +0900

    fix: throw exception and handle case when refresh token is missing in cookies

--- backend/chidic-common/src/main/kotlin/com/chidiccommon/exception/GlobalExceptionAdvice.kt
+++ backend/chidic-common/src/main/kotlin/com/chidiccommon/exception/GlobalExceptionAdvice.kt
@@ -1,5 +1,7 @@
 package com.chidiccommon.exception
 
+import com.chidiccommon.exception.exceptions.RefreshTokenNotFoundException
+import com.chidiccommon.exception.exceptions.UserNotFoundException
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -7,10 +9,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionAdvice {
-
-    @ExceptionHandler(Exception::class)
+    @ExceptionHandler(UserNotFoundException::class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     fun handleNotFoundException(e: RuntimeException): String? {
+        return e.message
+    }
+
+    @ExceptionHandler(RefreshTokenNotFoundException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleBadRequestException(e: RuntimeException): String? {
         return e.message
     }
 }
