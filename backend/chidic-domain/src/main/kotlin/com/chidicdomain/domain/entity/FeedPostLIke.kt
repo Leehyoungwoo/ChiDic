208795145f5c686775b139a02aca3a3e74497d96
dlguddn3024 <dlguddn3024@gmail.com>
2025-01-22 21:23:46 +0900

    feat: Add FeedPostLike Entity

---
+++ backend/chidic-domain/src/main/kotlin/com/chidicdomain/domain/entity/FeedPostLIke.kt
@@ -0,0 +1,34 @@
+package com.chidicdomain.domain.entity
+
+import com.chidicdomain.domain.entity.id.PostLikeId
+import jakarta.persistence.*
+
+@Entity
+class FeedPostLIke(
+    @EmbeddedId
+    val id: PostLikeId,
+
+    @Column(name = "is_liked", nullable = false)
+    var isLiked: Boolean = false
+) {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", insertable = false, updatable = false)
+    var feedPost: FeedPost? = null
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    var user: User? = null
+
+    fun associateFeedAndUser(feedPost: FeedPost, user: User) {
+        this.feedPost = feedPost
+        this.user = user
+    }
+
+    fun likePost() {
+        isLiked = true
+    }
+
+    fun unlikePost() {
+        isLiked = false
+    }
+}
