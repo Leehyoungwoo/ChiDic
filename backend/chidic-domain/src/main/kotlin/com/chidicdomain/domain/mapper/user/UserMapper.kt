fd6d67480850827bb1fad049231262cbedd88040
dlguddn3024 <dlguddn3024@gmail.com>
2025-02-01 20:48:40 +0900

    Refactor DTO, Request, and Response Structure for Better Separation of Concerns

--- backend/chidic-domain/src/main/kotlin/com/chidicdomain/domain/mapper/user/UserMapper.kt
+++ backend/chidic-domain/src/main/kotlin/com/chidicdomain/domain/mapper/user/UserMapper.kt
@@ -1,11 +1,14 @@
 package com.chidicdomain.domain.mapper.user
 
 import com.chidiccommon.dto.OAuth2UserInfo
-import com.chidiccommon.dto.UserInfoResponse
 import com.chidiccommon.enum.Provider
 import com.chidicdomain.domain.entity.User
+import com.chidicdomain.dto.UserInfoDto
+import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 
+@Mapper(componentModel = "spring")
 interface UserMapper {
-    fun toEntity(oAuth2UserInfo: OAuth2UserInfo, provider: Provider): User
-    fun toInfoDto(user: User): UserInfoResponse
+    fun toEntity(oAuth2UserInfo: OAuth2UserInfo): User
+    fun toInfoDto(user: User): UserInfoDto
 }
