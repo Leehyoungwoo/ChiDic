8552046dc0bcddbe01ab43b81aad3629703e6962
dlguddn3024 <dlguddn3024@gmail.com>
2025-02-03 17:46:22 +0900

    feat: Resolve N+1 query problem by optimizing data fetching with JOIN FETCH

--- backend/chidic-domain/src/main/kotlin/com/chidicdomain/domain/repository/FeedPostRepository.kt
+++ backend/chidic-domain/src/main/kotlin/com/chidicdomain/domain/repository/FeedPostRepository.kt
@@ -2,13 +2,23 @@ package com.chidicdomain.domain.repository
 
 import com.chidicdomain.domain.entity.FeedPost
 import com.chidicdomain.domain.entity.User
+import io.lettuce.core.dynamic.annotation.Param
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface FeedPostRepository : JpaRepository<FeedPost, Long>{
+    @Query("SELECT fp FROM FeedPost fp " +
+            "JOIN FETCH fp.user " +
+            "JOIN FETCH fp.comments c " +
+            "JOIN FETCH c.user " +
+            "WHERE fp.id = :id")
+    fun findFeedPostWithUserAndComments(@Param("id") id: Long): FeedPost
+
     fun findByUserIn(userList: List<User>, pageable: Pageable): List<FeedPost>
+
     fun findByUserInAndIdLessThan(userList: List<User>, lastFeedPostId: Long, pageable: Pageable): List<FeedPost>
 }
