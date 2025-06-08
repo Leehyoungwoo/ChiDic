45046512a95a2669dc30348d5d57ff88dd8eeed1
dlguddn3024 <dlguddn3024@gmail.com>
2025-06-08 12:57:22 +0900

    build(gradle): disable BootJar and enable plain Jar packaging

--- backend/chidic-common/build.gradle.kts
+++ backend/chidic-common/build.gradle.kts
@@ -1,7 +1,17 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
     kotlin("plugin.spring")
     id("org.springframework.boot")
     id("io.spring.dependency-management")
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = true
+}
+
+tasks.getByName<BootJar>("bootJar") {
+    enabled = false
 }
 
 dependencies {
--- backend/chidic-domain/build.gradle.kts
+++ backend/chidic-domain/build.gradle.kts
@@ -1,9 +1,19 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
     kotlin("plugin.spring")
     kotlin("plugin.jpa")
     id("org.springframework.boot")
     id("io.spring.dependency-management")
     kotlin("kapt")
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = true
+}
+
+tasks.getByName<BootJar>("bootJar") {
+    enabled = false
 }
 
 dependencies {
