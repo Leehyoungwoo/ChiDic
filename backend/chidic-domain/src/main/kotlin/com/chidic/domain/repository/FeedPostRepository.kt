b949c78daed73c974ba77c29fb6996f0169907d3
dlguddn3024 <dlguddn3024@gmail.com>
2025-08-04 15:54:18 +0900

    feat: add DB fallback query with cursor-based pagination

--- backend/chidic-domain/src/main/kotlin/com/chidic/domain/repository/FeedPostRepository.kt
+++ backend/chidic-domain/src/main/kotlin/com/chidic/domain/repository/FeedPostRepository.kt
@@ -9,27 +9,52 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface FeedPostRepository : JpaRepository<FeedPost, Long>{
-    @Query("SELECT fp FROM FeedPost fp " +
-            "JOIN FETCH fp.user " +
-            "LEFT JOIN FETCH fp.comments c " +
-            "LEFT JOIN FETCH c.user " +
-            "WHERE fp.id = :id")
-    fun findFeedPostWithUserAndComments(@Param("id") id: Long): FeedPost?
+interface FeedPostRepository : JpaRepository<FeedPost, Long> {
 
-    @Query("SELECT p FROM FeedPost p WHERE p.user IN :users AND p.id NOT IN :readPostIds ORDER BY p.id DESC")
-    fun findUnreadFeedPosts(users: List<User>, readPostIds: List<Long>, pageable: Pageable): List<FeedPost>
+    @Query(
+        """ 
+        SELECT fp FROM FeedPost fp 
+        JOIN FETCH fp.user 
+        LEFT JOIN FETCH fp . comments c 
+        LEFT JOIN FETCH c . user 
+        WHERE fp . id = :id
+        """
+    )
+    fun findFeedPostWithUserAndComments(
+        @Param("id") id: Long)
+    : FeedPost?
+
+    @Query(
+        """
+        SELECT f FROM FeedPost f
+        JOIN FETCH f.user
+        WHERE f.user IN :users
+          AND f.id < :lastId
+        """
+    )
+    fun findLatestPosts(
+        @Param("users") users: List<User>,
+        @Param("lastId") lastId: Long,
+        pageable: Pageable
+    ): List<FeedPost>
+
+
+
+//    @Query("SELECT p FROM FeedPost p WHERE p.user IN :users AND p.id NOT IN :readPostIds ORDER BY p.id DESC")
+//    fun findUnreadFeedPosts(users: List<User>, readPostIds: List<Long>, pageable: Pageable): List<FeedPost>
 
     fun findByUserIn(userList: List<User>, pageable: Pageable): List<FeedPost>
 
     fun findByUserInAndIdLessThan(userList: List<User>, lastFeedPostId: Long, pageable: Pageable): List<FeedPost>
 
-    @Query("""
-    SELECT f FROM FeedPost f 
-    LEFT JOIN FETCH f.user 
-    LEFT JOIN FETCH f.comments 
-    WHERE f.id IN :ids 
-    ORDER BY f.id DESC
-""")
+    @Query(
+        """
+                SELECT f FROM FeedPost f
+                LEFT JOIN FETCH f . user
+                LEFT JOIN FETCH f . comments
+                WHERE f . id IN :ids
+        ORDER BY f.id DESC
+                """
+    )
     fun findAllByIdIn(@Param("ids") ids: List<Long>): List<FeedPost>
 }
